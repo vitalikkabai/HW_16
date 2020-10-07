dbf5eef083faa276a9d2ba16e6562127a58f7d57
Vitalik Kabai <vitalikkabai@gmail.com>
2020-10-07 05:53:04 +0300

    Updates

---
+++ precache-manifest.3b12b8d41c5b4bd1dadab69498fd7e37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c52451b22fea9f77bd3e29d918f2a67",
+    "url": "/HW_16/index.html"
+  },
+  {
+    "revision": "c24dc2db4f882a7732ca",
+    "url": "/HW_16/static/css/main.4701425a.chunk.css"
+  },
+  {
+    "revision": "728a6b56aa02d4cf8463",
+    "url": "/HW_16/static/js/2.65ff6f06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW_16/static/js/2.65ff6f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c24dc2db4f882a7732ca",
+    "url": "/HW_16/static/js/main.dd2ed91c.chunk.js"
+  },
+  {
+    "revision": "13cf55fc4ba9dae4f1d7",
+    "url": "/HW_16/static/js/runtime-main.47147651.js"
+  }
+]);
